9b56df3f55081776c88d6dd097fd288eea48dba0
E. M <malai8220.g@gmail.com>
2026-08-25 14:39:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -174,17 +174,15 @@ for (
 ===================================================== */
 
 const photos = [
-
-    "images/photo1.jpg",
-    "images/photo2.jpg",
-    "images/photo3.jpg",
-    "images/photo4.jpg",
-    "images/photo5.jpg",
-    "images/photo6.jpg",
-    "images/photo7.jpg",
-    "images/photo8.jpg",
-    "images/photo9.jpg"
-
+    "photo1.jpg",
+    "photo2.jpg",
+    "photo3.jpg",
+    "photo4.jpg",
+    "photo5.jpg",
+    "photo6.jpg",
+    "photo7.jpg",
+    "photo8.jpg",
+    "photo9.jpg"
 ];
 
 
